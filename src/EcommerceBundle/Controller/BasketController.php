2c275c004d61d3c92c2a4c6bd4406b6b04e64e4d
Stephen <stephenfauvergue@gmail.com>
2018-10-10 16:46:57 +0200

    front-office views added

--- src/EcommerceBundle/Controller/BasketController.php
+++ src/EcommerceBundle/Controller/BasketController.php
@@ -6,5 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BasketController extends Controller
 {
+    public function viewAction()
+    {
+        return $this->render('EcommerceBundle:Basket:view.html.twig');
+    }
 
+    public function deliveryAction()
+    {
+        return $this->render('EcommerceBundle:Basket:delivery.html.twig');
+    }
+
+    public function validationAction()
+    {
+        return $this->render('EcommerceBundle:Basket:validation.html.twig');
+    }
 }
--- src/EcommerceBundle/Controller/ProductController.php
+++ src/EcommerceBundle/Controller/ProductController.php
@@ -6,5 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProductController extends Controller
 {
+    public function indexAction()
+    {
+        return $this->render('EcommerceBundle:Product:index.html.twig');
+    }
+
+    public function viewAction()
+    {
+        return $this->render('EcommerceBundle:Product:view.html.twig');
+    }
 
 }
